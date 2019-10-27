d02cceaa07b54b1deab602c54245cb8227391ba5
Ilham Gumantung Gusti <iggusti@gmail.com>
2019-10-27 14:57:27 +0700

    Auth

---
+++ app/src/main/java/com/example/latihan/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.latihan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Deklarasi Variable
+    private ProgressBar progressBar;
+    private EditText NIM, Nama, Jurusan;
+    private FirebaseAuth auth;
+    private Button Logout, Simpan, Login, ShowData;
+
+    //Membuat Kode Permintaan
+    private int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.GONE);
+
+        //Inisialisasi ID (Button)
+        Logout = findViewById(R.id.logout);
+        Logout.setOnClickListener(this);
+        Simpan = findViewById(R.id.save);
+        Simpan.setOnClickListener(this);
+        Login = findViewById(R.id.login);
+        Login.setOnClickListener(this);
+        ShowData = findViewById(R.id.showdata);
+        ShowData.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance(); //Mendapakan Instance Firebase Autentifikasi
+
+        //Inisialisasi ID (EditText)
+        NIM = findViewById(R.id.nim);
+        Nama = findViewById(R.id.nama);
+        Jurusan = findViewById(R.id.jurusan);
+
+        /*
+         * Mendeteksi apakah ada user yang masuk, Jika tidak, maka setiap komponen UI akan dinonaktifkan
+         * Kecuali Tombol Login. Dan jika ada user yang terautentikasi, semua fungsi/komponen
+         * didalam User Interface dapat digunakan, kecuali tombol Logout
+         */
+        if(auth.getCurrentUser() == null){
+            defaultUI();
+        }else {
+            updateUI();
+        }
+    }
+
+    //Tampilan Default pada Activity jika user belum terautentikasi
+    private void defaultUI(){
+        Logout.setEnabled(false);
+        Simpan.setEnabled(false);
+        ShowData.setEnabled(false);
+        Login.setEnabled(true);
+        NIM.setEnabled(false);
+        Nama.setEnabled(false);
+        Jurusan.setEnabled(false);
+    }
+
+    //Tampilan User Interface pada Activity setelah user Terautentikasi
+    private void updateUI(){
+        Logout.setEnabled(true);
+        Simpan.setEnabled(true);
+        Login.setEnabled(false);
+        ShowData.setEnabled(true);
+        NIM.setEnabled(true);
+        Nama.setEnabled(true);
+        Jurusan.setEnabled(true);
+        progressBar.setVisibility(View.GONE);
+    }
+
+    // Mengecek apakah ada data yang kosong, akan digunakan pada Tutorial Selanjutnya
+    private boolean isEmpty(String s){
+        return TextUtils.isEmpty(s);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN adalah kode permintaan yang Anda berikan ke startActivityForResult, saat memulai masuknya arus.
+        if (requestCode == RC_SIGN_IN) {
+
+            //Berhasil masuk
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                updateUI();
+            }else {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Login Dibatalkan", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                // Statement program untuk login/masuk
+                startActivityForResult(AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+
+                                //Memilih Provider atau Method masuk yang akan kita gunakan
+                                .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                .setIsSmartLockEnabled(false)
+                                .build(),
+                        RC_SIGN_IN);
+                progressBar.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.save:
+                /*
+                  Digunakan untuk mendapatkan referensi dan meyimpan data pada Database
+                  Akan dibahas pada Tutorial Berikutnya, Mengenai Fungsi Create
+                 */
+                break;
+
+            case R.id.logout:
+                // Statement program untuk logout/keluar
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+                                Toast.makeText(MainActivity.this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        });
+                break;
+            case R.id.showdata:
+                // Digunakan untuk melihat data yang sudah tersimpan didalam Database
+                // Akan digunakan pada Tutorial Berikutnya, mengenai penggunaan Fungsi Read
+                break;
+        }
+    }
+}
